f69f6a0e0fc9b82bef86021616c80c6d63eb130f
Aleksandr Loginov <loganche@Aleksandrs-MacBook-Pro.local>
2025-04-01 19:48:49 +0300

    refactored Game file

--- stateMachine/states/game.lua
+++ stateMachine/states/game.lua
@@ -4,11 +4,6 @@ local Game = {}
 
 local Player = require("game.playerEntity")
 local Enemy = require("game.enemyEntity")
-local Bullet = require("game.bullet")
-
-PlayerBullets = {}
-SnakeBullets = {}
-
 
 function Game:enter()
     self.player = Player(300, 20, 500, 100,
@@ -23,48 +18,21 @@ function Game:update(dt)
     self.player:update(dt)
     self.enemy:update(dt)
 
-    if self.player.health <= 0 or self.enemy.health <= 0 then
-        Result['player'] = self.player.health
-        Result['enemy'] = self.enemy.health
-        for i, _ in pairs(self.player.bullets) do self.player.bullets[i] = nil end
-        for i, _ in pairs(self.enemy.bullets) do self.enemy.bullets[i] = nil end
-        StateMachine:nextState()
-    end
+    self:isFinished()
 
-    healthDelta = 20
-    speedDelta = 50
-    for i, v in ipairs(self.player.bullets) do
-        v:update(dt)
-        if v:checkCollision(self.enemy) then
-            v:collisionSideEffects(self.enemy, healthDelta, speedDelta)
-        end
-
-        if v.dead then
-            table.remove(self.player.bullets, i)
-        end
-    end
-
-    for i, v in ipairs(self.enemy.bullets) do
-        v:update(dt)
-        if v:checkCollision(self.player) then
-            v:collisionSideEffects(self.player, healthDelta, speedDelta)
-        end
-
-        if v.dead then
-            table.remove(self.enemy.bullets, i)
-        end
-    end
+    self:bulletShot(dt, self.player.bullets, self.enemy, 20, 50)
+    self:bulletShot(dt, self.enemy.bullets, self.player, 20, 0)
 end
 
 function Game:draw()
     self.player:draw()
     self.enemy:draw()
 
-    for i, v in ipairs(self.player.bullets) do
+    for _, v in ipairs(self.player.bullets) do
         v:draw()
     end
 
-    for i, v in ipairs(self.enemy.bullets) do
+    for _, v in ipairs(self.enemy.bullets) do
         v:draw()
     end
 end
@@ -84,6 +52,29 @@ end
 
 function Game:__tostring()
     return 'Game', 'Next'
+end
+
+function Game:bulletShot(dt, bullets, entity, healthDelta, speedDelta)
+    for i, v in ipairs(bullets) do
+        v:update(dt)
+        if v:checkCollision(entity) then
+            v:collisionSideEffects(entity, healthDelta, speedDelta)
+        end
+
+        if v.dead then
+            table.remove(bullets, i)
+        end
+    end
+end
+
+function Game:isFinished()
+    if self.player.health <= 0 or self.enemy.health <= 0 then
+        Result['player'] = self.player.health
+        Result['enemy'] = self.enemy.health
+        for i, _ in pairs(self.player.bullets) do self.player.bullets[i] = nil end
+        for i, _ in pairs(self.enemy.bullets) do self.enemy.bullets[i] = nil end
+        StateMachine:nextState()
+    end
 end
 
 return Game
